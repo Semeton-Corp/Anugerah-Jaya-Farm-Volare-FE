27be8dd484df8eca40e141e0f6ed258238a12d4a
mayunngrh <mayunngrh@gmail.com>
2025-04-05 01:59:16 +0800

    make the login page responsive

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,13 +11,19 @@ const Login = () => {
   }
 
   return (
-    <div className="relative flex h-screen w-screen">
+    <div className="relative h-screen w-screen">
       {/* left login item */}
-      <div className="w-1/2 h-full flex flex-col justify-center p-28">
+      <div className="absolute left-0 w-full md:w-1/2 h-full flex flex-col  md:justify-center md:p-16 pt-16 px-12 z-10 mt-48 md:mt-0 rounded-t-3xl bg-white">
         {/* logo & company name */}
         <div className="flex items-center mb-8">
-          <img src={logo} alt="AJF Logo" className="w-24 h-24 mr-4" />
-          <h1 className="text-2xl font-bold w-60">Anugerah Jaya Farm</h1>
+          <img
+            src={logo}
+            alt="AJF Logo"
+            className="md:w-24 md:h-24 w-14 h-14 mr-4"
+          />
+          <h1 className="md:text-2xl text-base font-bold md:w-60 w-35+">
+            Anugerah Jaya Farm
+          </h1>
         </div>
         {/* welcoming text */}
         <div className="mb-8">
@@ -73,7 +79,7 @@ const Login = () => {
         </div>
       </div>
       {/* right login item */}
-      <div className="w-1/2 h-full">
+      <div className="w-full top-0 md:absolute right-0 sm:top-0 md:z-0 md:w-1/2 h-[40%] md:h-full">
         <img
           src="/photo_login_page.png"
           alt="Login Page Photo"
